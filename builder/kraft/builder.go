b6c3bbf766b3eba2b3cb83689fe8fcf0eb1f6584
Cezar Craciunoiu <cezar.craciunoiu@gmail.com>
2023-01-04 12:02:49 +0200

    builder/kraft: Register steps to the builder
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

--- builder/kraft/builder.go
+++ builder/kraft/builder.go
@@ -6,19 +6,13 @@ import (
 	"context"
 
 	"github.com/hashicorp/hcl/v2/hcldec"
-	"github.com/hashicorp/packer-plugin-sdk/common"
 	"github.com/hashicorp/packer-plugin-sdk/multistep"
 	"github.com/hashicorp/packer-plugin-sdk/multistep/commonsteps"
 	"github.com/hashicorp/packer-plugin-sdk/packer"
 	"github.com/hashicorp/packer-plugin-sdk/template/config"
 )
 
-const BuilderId = "scaffolding.builder"
-
-type Config struct {
-	common.PackerConfig `mapstructure:",squash"`
-	MockOption          string `mapstructure:"mock"`
-}
+const BuilderId = "kraft.builder"
 
 type Builder struct {
 	config Config
@@ -29,7 +23,7 @@ func (b *Builder) ConfigSpec() hcldec.ObjectSpec { return b.config.FlatMapstruct
 
 func (b *Builder) Prepare(raws ...interface{}) (generatedVars []string, warnings []string, err error) {
 	err = config.Decode(&b.config, &config.DecodeOpts{
-		PluginType:  "packer.builder.scaffolding",
+		PluginType:  "packer.builder.kraft",
 		Interpolate: true,
 	}, raws...)
 	if err != nil {
@@ -37,37 +31,34 @@ func (b *Builder) Prepare(raws ...interface{}) (generatedVars []string, warnings
 	}
 	// Return the placeholder for the generated data that will become available to provisioners and post-processors.
 	// If the builder doesn't generate any data, just return an empty slice of string: []string{}
-	buildGeneratedData := []string{
-		"GeneratedMockData",
-	}
+	buildGeneratedData := []string{}
 	return buildGeneratedData, warnings, nil
 }
 
 func (b *Builder) Run(ctx context.Context, ui packer.Ui, hook packer.Hook) (packer.Artifact, error) {
-	steps := []multistep.Step{}
-
-	steps = append(steps,
-		&StepSayConfig{
-			MockConfig: b.config.MockOption,
-		},
+	driver := &KraftDriver{Ctx: &b.config.ctx, Ui: ui}
+	steps := []multistep.Step{
+		&StepPkgSource{},
+		&StepPkgUpdate{},
+		&StepPkgPull{},
+		&StepSet{},
+		&StepBuild{},
 		new(commonsteps.StepProvision),
-	)
+	}
 
 	// Setup the state bag and initial state for the steps
 	state := new(multistep.BasicStateBag)
 	state.Put("hook", hook)
 	state.Put("ui", ui)
 
-	// Set the value of the generated data that will become available to provisioners.
-	// To share the data with post-processors, use the StateData in the artifact.
-	state.Put("generated_data", map[string]interface{}{
-		"GeneratedMockData": "mock-build-data",
-	})
-
-	// generatedData := &packerbuilderdata.GeneratedData{State: state}
+	state.Put("config", &b.config)
+	state.Put("driver", driver)
 
 	// Run!
 	b.runner = commonsteps.NewRunner(steps, b.config.PackerConfig, ui)
+	if b.runner == nil {
+		return nil, nil
+	}
 	b.runner.Run(ctx, state)
 
 	// If there was an error, return that
@@ -80,7 +71,7 @@ func (b *Builder) Run(ctx context.Context, ui packer.Ui, hook packer.Hook) (pack
 	artifact := &Artifact{
 		// Add the builder generated data to the artifact StateData so that post-processors
 		// can access them.
-		StateData: map[string]interface{}{"generated_data": state.Get("generated_data")},
+		// StateData: map[string]interface{}{"generated_data": state.Get("generated_data")},
 	}
 	return artifact, nil
 }
