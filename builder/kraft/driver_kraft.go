e2d985e9b84b8628237749c112de7712304fe798
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2023-01-05 15:01:29 +0200

    builder/kraft: Switch to calling the kraft implementation directly
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

--- builder/kraft/driver_kraft.go
+++ builder/kraft/driver_kraft.go
@@ -1,123 +1,63 @@
 package kraft
 
 import (
-	"bytes"
 	"fmt"
-	"log"
-	"os/exec"
-	"sync"
 
 	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
 	"github.com/hashicorp/packer-plugin-sdk/template/interpolate"
+	"github.com/spf13/cobra"
+
+	kraftbuild "kraftkit.sh/cmd/kraft/build"
+	kraftpull "kraftkit.sh/cmd/kraft/pkg/pull"
+	kraftsource "kraftkit.sh/cmd/kraft/pkg/source"
+	kraftupdate "kraftkit.sh/cmd/kraft/pkg/update"
+	kraftset "kraftkit.sh/cmd/kraft/set"
 )
 
-// TODO: Add implementation + call to each kraft command
 type KraftDriver struct {
 	Ui  packersdk.Ui
 	Ctx *interpolate.Context
 
-	// The directory Docker should use to store its client configuration.
-	// Provides an isolated client configuration to each Docker operation to
-	// prevent race conditions.
-	ConfigDir string
-
-	l sync.Mutex
+	CommandContext *cobra.Command
 }
 
 func (d *KraftDriver) Build(path, architecture, platform string) error {
-	var stderr bytes.Buffer
-	cmd := exec.Command("kraft", "build", "-m", architecture, "-p", platform, path)
-	cmd.Stderr = &stderr
-
-	log.Printf("Building image: %s", path)
-	if err := cmd.Start(); err != nil {
-		return err
+	build := kraftbuild.Build{
+		Architecture: architecture,
+		Platform:     platform,
 	}
-
-	if err := cmd.Wait(); err != nil {
-		err = fmt.Errorf("error building image: %s\nStderr: %s",
-			err, stderr.String())
-		return err
-	}
-
-	return nil
+	return build.Run(d.CommandContext, []string{path})
 }
 
 func (d *KraftDriver) Pull(source, workdir string) error {
-	var stderr bytes.Buffer
-	cmd := exec.Command("kraft", "pkg", "pull", "-w", workdir, source)
-	cmd.Stderr = &stderr
-
-	log.Printf("Pulling image: %s", source)
-	if err := cmd.Start(); err != nil {
-		return err
+	pull := kraftpull.Pull{
+		Workdir: workdir,
 	}
 
-	if err := cmd.Wait(); err != nil {
-		err = fmt.Errorf("error pulling image: %s\nStderr: %s",
-			err, stderr.String())
-		return err
-	}
-
-	return nil
+	return pull.Run(d.CommandContext, []string{source})
 }
 
 func (d *KraftDriver) Source(source string) error {
-	var stderr bytes.Buffer
-	cmd := exec.Command("kraft", "pkg", "source", source)
-	cmd.Stderr = &stderr
+	src := kraftsource.Source{}
 
-	log.Printf("Sourcing image: %s", source)
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-
-	if err := cmd.Wait(); err != nil {
-		err = fmt.Errorf("error sourcing image: %s\nStderr: %s",
-			err, stderr.String())
-		return err
-	}
-	return nil
+	return src.Run(d.CommandContext, []string{source})
 }
 
 func (d *KraftDriver) Update() error {
-	var stderr bytes.Buffer
-	cmd := exec.Command("kraft", "pkg", "update")
-	cmd.Stderr = &stderr
-
-	log.Printf("Updating sources")
-	if err := cmd.Start(); err != nil {
-		return err
+	update := kraftupdate.Update{
+		Manager: "manifest",
 	}
 
-	if err := cmd.Wait(); err != nil {
-		err = fmt.Errorf("error updating sources: %s\nStderr: %s",
-			err, stderr.String())
-		return err
-	}
-	return nil
+	return update.Run(d.CommandContext, []string{})
 }
 
 func (d *KraftDriver) Set(options map[string]string) error {
-	var strOptions []string = []string{"set"}
-	var stderr bytes.Buffer
+	set := kraftset.Set{}
+	opts := []string{}
 
-	for key, value := range options {
-		strOptions = append(strOptions, fmt.Sprintf("%s=%s", key, value))
+	for k, v := range options {
+		opts = append(opts, fmt.Sprintf("%s=%s", k, v))
 	}
 
-	cmd := exec.Command("kraft", strOptions...)
-	cmd.Stderr = &stderr
-
-	log.Printf("Setting custom symbols image")
-	if err := cmd.Start(); err != nil {
-		return err
-	}
-
-	if err := cmd.Wait(); err != nil {
-		err = fmt.Errorf("error setting symbols: %s\nStderr: %s",
-			err, stderr.String())
-		return err
-	}
-	return nil
+	return set.Run(d.CommandContext, opts)
 }
