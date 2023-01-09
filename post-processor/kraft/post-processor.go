a5d264bf284ed49a919c53e858dda7ad7bdb4353
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2023-01-09 19:56:05 +0200

    post-process/kraft: Add post processor for creating initram
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

--- post-processor/kraft/post-processor.go
+++ post-processor/kraft/post-processor.go
@@ -1,21 +1,19 @@
-package kraft
+package kraftpprocessor
 
 import (
+	"bytes"
 	"context"
 	"fmt"
+	"io/ioutil"
+	"os/exec"
+	"packer-plugin-kraft/builder/kraft"
 
 	"github.com/hashicorp/hcl/v2/hcldec"
-	"github.com/hashicorp/packer-plugin-sdk/common"
 	packersdk "github.com/hashicorp/packer-plugin-sdk/packer"
 	"github.com/hashicorp/packer-plugin-sdk/template/config"
 	"github.com/hashicorp/packer-plugin-sdk/template/interpolate"
+	"github.com/mitchellh/mapstructure"
 )
-
-type Config struct {
-	common.PackerConfig `mapstructure:",squash"`
-	MockOption          string `mapstructure:"mock"`
-	ctx                 interpolate.Context
-}
 
 type PostProcessor struct {
 	config Config
@@ -25,7 +23,7 @@ func (p *PostProcessor) ConfigSpec() hcldec.ObjectSpec { return p.config.FlatMap
 
 func (p *PostProcessor) Configure(raws ...interface{}) error {
 	err := config.Decode(&p.config, &config.DecodeOpts{
-		PluginType:         "packer.post-processor.scaffolding",
+		PluginType:         "packer.post-processor.kraft",
 		Interpolate:        true,
 		InterpolateContext: &p.config.ctx,
 		InterpolateFilter: &interpolate.RenderFilter{
@@ -39,6 +37,68 @@ func (p *PostProcessor) Configure(raws ...interface{}) error {
 }
 
 func (p *PostProcessor) PostProcess(ctx context.Context, ui packersdk.Ui, source packersdk.Artifact) (packersdk.Artifact, bool, bool, error) {
-	ui.Say(fmt.Sprintf("post-processor mock: %s", p.config.MockOption))
-	return source, true, true, nil
+	switch source.BuilderId() {
+	case kraft.BuilderId, BuilderId:
+		break
+	default:
+		return nil, false, false, fmt.Errorf("unknown artifact %s", source.BuilderId())
+	}
+
+	var binaries []string
+	if err := mapstructure.Decode(source.State("binaries"), &binaries); err != nil {
+		err := fmt.Errorf("failed to decode binaries")
+		ui.Error(err.Error())
+		return source, false, false, err
+	}
+
+	var stdout bytes.Buffer
+	var stderr bytes.Buffer
+
+	args := []string{p.config.FileSource, "-depth", "-print"}
+
+	cmd := exec.Command("find", args...)
+	cmd.Stdout = &stdout
+	cmd.Stderr = &stderr
+
+	if err := cmd.Start(); err != nil {
+		return nil, false, false, err
+	}
+
+	if err := cmd.Wait(); err != nil {
+		err = fmt.Errorf("error listing files: %s\nStderr: %s",
+			err, stderr.String())
+		return nil, false, false, err
+	}
+
+	var stdout2 bytes.Buffer
+	var stderr2 bytes.Buffer
+
+	// Pipe the output to the archiver
+	cmd2 := exec.Command("bsdcpio", []string{"-o", "--format", "newc"}...)
+	cmd2.Stdout = &stdout2
+	cmd2.Stderr = &stderr2
+	cmd2.Stdin = &stdout
+
+	if err := cmd2.Start(); err != nil {
+		return nil, false, false, err
+	}
+
+	if err := cmd2.Wait(); err != nil {
+		err = fmt.Errorf("error archiving files: %s\nStderr: %s",
+			err, stderr.String())
+		return nil, false, false, err
+	}
+
+	// Write the output from inside stdout2 into a file
+	if err := ioutil.WriteFile(p.config.FileDestination, stdout2.Bytes(), 0644); err != nil {
+		return nil, false, false, err
+	}
+
+	// Update the artifact with the new file
+	artifact := &kraft.Artifact{
+		StateData: map[string]interface{}{
+			"initramfs": p.config.FileDestination,
+		},
+	}
+	return artifact, true, true, nil
 }
--- post-processor/kraft/post-processor_acc_test.go
+++ post-processor/kraft/post-processor_acc_test.go
@@ -1,12 +1,9 @@
-package kraft
+package kraftpprocessor
 
 import (
 	_ "embed"
 	"fmt"
-	"io/ioutil"
-	"os"
 	"os/exec"
-	"regexp"
 	"testing"
 
 	"github.com/hashicorp/packer-plugin-sdk/acctest"
@@ -18,7 +15,7 @@ var testPostProcessorHCL2Basic string
 // Run with: PACKER_ACC=1 go test -count 1 -v ./post-processor/scaffolding/post-processor_acc_test.go  -timeout=120m
 func TestAccScaffoldingPostProcessor(t *testing.T) {
 	testCase := &acctest.PluginTestCase{
-		Name: "scaffolding_post-processor_basic_test",
+		Name: "kraft_post-processor_basic_test",
 		Setup: func() error {
 			return nil
 		},
@@ -26,30 +23,26 @@ func TestAccScaffoldingPostProcessor(t *testing.T) {
 			return nil
 		},
 		Template: testPostProcessorHCL2Basic,
-		Type:     "scaffolding-my-post-processor",
+		Type:     "kraft-post-processor",
 		Check: func(buildCommand *exec.Cmd, logfile string) error {
 			if buildCommand.ProcessState != nil {
 				if buildCommand.ProcessState.ExitCode() != 0 {
-					return fmt.Errorf("Bad exit code. Logfile: %s", logfile)
+					return fmt.Errorf("bad exit code. Logfile: %s", logfile)
 				}
 			}
 
-			logs, err := os.Open(logfile)
-			if err != nil {
-				return fmt.Errorf("Unable find %s", logfile)
-			}
-			defer logs.Close()
+			// logs, err := os.Open(logfile)
+			// if err != nil {
+			// 	return fmt.Errorf("unable find %s", logfile)
+			// }
+			// defer logs.Close()
 
-			logsBytes, err := ioutil.ReadAll(logs)
-			if err != nil {
-				return fmt.Errorf("Unable to read %s", logfile)
-			}
-			logsString := string(logsBytes)
+			// logsBytes, err := ioutil.ReadAll(logs)
+			// if err != nil {
+			// 	return fmt.Errorf("unable to read %s", logfile)
+			// }
+			// logsString := string(logsBytes)
 
-			postProcessorOutputLog := "post-processor mock: my-mock-config"
-			if matched, _ := regexp.MatchString(postProcessorOutputLog+".*", logsString); !matched {
-				t.Fatalf("logs doesn't contain expected foo value %q", logsString)
-			}
 			return nil
 		},
 	}
