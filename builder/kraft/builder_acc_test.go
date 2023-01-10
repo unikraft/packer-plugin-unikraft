c0f1e16a10e7474022b6bbb474b2a0b8d1499369
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2023-01-10 15:22:51 +0200

    builder/kraft: Rename test
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

--- builder/kraft/builder_acc_test.go
+++ builder/kraft/builder_acc_test.go
@@ -18,7 +18,7 @@ var testBuilderHCL2Basic string
 // Run with: PACKER_ACC=1 go test -count 1 -v ./builder/scaffolding/builder_acc_test.go  -timeout=120m
 func TestAccScaffoldingBuilder(t *testing.T) {
 	testCase := &acctest.PluginTestCase{
-		Name: "scaffolding_builder_basic_test",
+		Name: "kraft_builder_basic_test",
 		Setup: func() error {
 			return nil
 		},
@@ -26,7 +26,7 @@ func TestAccScaffoldingBuilder(t *testing.T) {
 			return nil
 		},
 		Template: testBuilderHCL2Basic,
-		Type:     "scaffolding-my-builder",
+		Type:     "kraft-builder",
 		Check: func(buildCommand *exec.Cmd, logfile string) error {
 			if buildCommand.ProcessState != nil {
 				if buildCommand.ProcessState.ExitCode() != 0 {
@@ -46,7 +46,7 @@ func TestAccScaffoldingBuilder(t *testing.T) {
 			}
 			logsString := string(logsBytes)
 
-			buildGeneratedDataLog := "scaffolding-my-builder.basic-example: build generated data: mock-build-data"
+			buildGeneratedDataLog := "kraft-builder.basic-example: build generated data: mock-build-data"
 			if matched, _ := regexp.MatchString(buildGeneratedDataLog+".*", logsString); !matched {
 				t.Fatalf("logs doesn't contain expected foo value %q", logsString)
 			}
