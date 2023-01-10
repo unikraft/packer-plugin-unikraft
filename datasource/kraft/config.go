5f0d686180e6cba026632e58c1eef53ceffa2330
Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>
2023-01-10 15:23:39 +0200

    builder/kraft: Add config for datasources
    Signed-off-by: Cezar Craciunoiu <cezar.craciunoiu@unikraft.io>

---
+++ datasource/kraft/config.go
@@ -0,0 +1,15 @@
+//go:generate packer-sdc mapstructure-to-hcl2 -type Config
+
+package kraftdata
+
+import (
+	"github.com/hashicorp/packer-plugin-sdk/template/interpolate"
+)
+
+type Config struct {
+	ctx interpolate.Context
+}
+
+type DatasourceOutput struct {
+	Map map[string]string `mapstructure:"map"`
+}
